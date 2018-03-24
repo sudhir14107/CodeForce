ccdb04d2ba2256090d3562a0b6b7e3350d007847
sudhir14107 <sudhir14107@iiitd.ac.in>
2018-03-25 00:35:25 +0530

    just checking second commit

--- src/Area.java
+++ src/Area.java
@@ -4,7 +4,7 @@ public class Area {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//this is just sample code
+		//sudhir you are just starting
 		Scanner in=new Scanner(System.in);
 		int n=in.nextInt();
 		double h=in.nextInt();
